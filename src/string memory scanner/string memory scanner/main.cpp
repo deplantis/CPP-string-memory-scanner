1b1b3f9a7999ff91d7b324e38f6a2a4da6056e65
Deplantis <68348696+deplantis@users.noreply.github.com>
2023-09-12 22:00:48 +0200

    Update main.cpp

--- src/string memory scanner/string memory scanner/main.cpp
+++ src/string memory scanner/string memory scanner/main.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	
-	scanner scanscan(4520);
-	scanscan.scanstring("cat");
+	scanner scanscan(4520); // procress id
+	scanscan.scanstring("cat"); // the string that need to be founded
 
 	std::cin.get();
 	
