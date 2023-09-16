4166e7b1d0a8df92c9925b81326e0365faf55a58
Deplantis <68348696+deplantis@users.noreply.github.com>
2023-09-16 22:20:28 +0200

    Update main.cpp

--- src/string memory scanner/string memory scanner/main.cpp
+++ src/string memory scanner/string memory scanner/main.cpp
@@ -1,11 +1,20 @@
-#include "..\string memory scanner\scanner\scanner.h"
+nclude "..\string memory scanner\scanner\scanner.h"
 
 int main()
 {
 	
-	scanner scanscan(4520); // procress id
-	scanscan.scanstring("cat"); // the string that need to be founded
+	scanner scanscan(6372); // procress id
+	std::string stringtofind = "cat"; // the string that need to be founded
+	scanscan.scanstring(stringtofind); 
 
+	// loop all addreses that the scanner found
+	int count = 0;
+	for (auto& addressaddress : scanscan.returnaddreses())
+	{
+		count++;
+		std::cout << "string " << stringtofind << " found at address " << std::hex << addressaddress << std::dec << " count [" << count << "] \n";
+	}
+	std::cout << count << " strings found";
 	std::cin.get();
 	
 }
