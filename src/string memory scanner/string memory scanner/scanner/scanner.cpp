1621e7ee546c0d68743d60b38e761c7f7fd79697
Deplantis <68348696+deplantis@users.noreply.github.com>
2024-12-09 15:44:30 +0100

    Update scanner.cpp

--- src/string memory scanner/string memory scanner/scanner/scanner.cpp
+++ src/string memory scanner/string memory scanner/scanner/scanner.cpp
@@ -54,7 +54,6 @@ void scanner::scanstring(std::string& stringtofind)
 				{
 				//	std::cout << "Unicode string found \n";
 					addres.push_back((uintptr_t)currentmemorypage + begin);
-
 				}
 
 					}
